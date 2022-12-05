02590dbd618e42a6134f703acd9f4a9f3c980283
Konstantin <>
2022-12-05 21:29:22 +0300

    create array

--- Project/Program.cs
+++ Project/Program.cs
@@ -1,2 +1,29 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача: Написать программу, которая из имеющегося массива
+// строк, формирует массив из строк, длина которых меньше либо
+// равна 3 символа. Первоначальный массив можно ввести с клавиатуры,
+// либо задать на старте выполнения алгоритма. При решении не рекомендуется
+// пользоваться коллекциями, лучше обойтись исключительно массивами.
+
+// Примеры:
+// ["hello", "2", "world", ":-)"] -> ["2", ":-)"]
+// ["1234", "1567", "-2", "computer science"] -> ["-2"]
+// ["Russia", "Denmark", "Kazan"] -> []
+
+
+string[] array = new string[6] { "hello", "2", "world", ":-)", "computer science", "-2" };
+
+void PrintArray(string[] array)
+{
+
+    Console.Write("[");
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (i < array.Length - 1) Console.Write($"{array[i]}, ");
+        else Console.Write($"{array[i]}");
+    }
+    Console.WriteLine("]");
+
+}
+
+PrintArray(array);
+

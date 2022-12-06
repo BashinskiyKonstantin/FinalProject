36b018370ded3a29f7d509024f17e297e7123869
Konstantin <>
2022-12-06 21:47:52 +0300

    create diagram

--- Project/Program.cs
+++ Project/Program.cs
@@ -1,16 +1,4 @@
-﻿// Задача: Написать программу, которая из имеющегося массива
-// строк, формирует массив из строк, длина которых меньше либо
-// равна 3 символа. Первоначальный массив можно ввести с клавиатуры,
-// либо задать на старте выполнения алгоритма. При решении не рекомендуется
-// пользоваться коллекциями, лучше обойтись исключительно массивами.
-
-// Примеры:
-// ["hello", "2", "world", ":-)"] -> ["2", ":-)"]
-// ["1234", "1567", "-2", "computer science"] -> ["-2"]
-// ["Russia", "Denmark", "Kazan"] -> []
-
-
-string[] array = new string[6] { "hello", "2", "world", ":-)", "computer science", "-2" };
+﻿string[] array = new string[6] { "hello", "2", "world", ":-)", "computer science", "-2" };
 
 void PrintArray(string[] array)
 {
@@ -22,14 +10,13 @@ void PrintArray(string[] array)
     Console.WriteLine("]");
 }
 
-void ChangeArray(string[] array)
+void ChangeArray(string[] array)    
 {
     for (int i = 0; i < array.Length; i++)
     {
         if (array[i].Length > 3) array[i] = string.Empty;
     }
 }
-
 
 PrintArray(array);
 Console.Write("Результат нового массива: ");
